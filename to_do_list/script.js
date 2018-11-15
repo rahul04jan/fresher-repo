7df833af453f8a1aacbb1113bfad88a8fd7285ea
Rahul Srivastava <rahul.srivastava@cybergindia.local>
2018-11-15 16:09:01 +0530

    To Do Enhanced

--- to_do_list/script.js
+++ to_do_list/script.js
@@ -1,32 +1,71 @@
-var j=0;
-var m=0;
-var n=0,flg=0;
+var j=m=n=ids;
+var flg=0;
 var input;
 var ids=0;
+var edit_flag=0;
+var mid=0,mid1=0;
+ var edt_date;
+    var edt_typeid;
 function f1(){
-	input = document.getElementById("text");
+  input = document.getElementById("text");
 input.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
         document.getElementById("mybtn").click();
     }
-});	
+}); 
 }
 
 function add () {
-	
-var ul = document.getElementById("result");
+   var inrval=document.getElementById("text").value;
+    var ts = new Date();
+  if (edit_flag==1){
+
+    var inrval=document.getElementById("text").value;
+     console.log("himid"+mid1);
+   var type=document.getElementById("select_type").value;
+    console.log(type)
+    if  (type=='Daily Task'){
+        var type_val=1;
+    }
+    else  if  (type=='Weekend Task'){
+        var type_val=2;
+    }
+    else{
+     var type_val=null;
+    }
+    console.log("typval"+type_val);
+   
+
+            
+    $.ajax({
+    type: "PUT",
+    url: 'http://localhost:56830/api/Todoes/'+ids,
+    async:true, 
+    dataType:"json",
+    data:{"Id":ids,"Description":inrval,"Status":"1","Created_At":ts.toISOString(),"Type_Id":type_val},
+    crossDomain:true,
+    success: function(response) {
+       
+       console.log(response);
+      mid=ids;
+      console.log("responsemid"+mid);
+      var ul = document.getElementById("result");
 var candidate = document.getElementById("text").value;
 if (candidate==""){
-	alert("You must write Something!");
+  alert("You must write Something!");
 }
 else{
 var li = document.createElement("li");
 if (ids!=0){
-	var li_id="li"+ids;
+  
+  var li_id="li"+ids ;
+  console.log(ids);
 }
 else{
-var li_id="li"+j++;
+ 
+var li_id="li"+ mid;
+console.log("responsemid"+mid);
 }
 li.setAttribute('id',li_id);
 li.setAttribute('class',"a");
@@ -57,116 +96,286 @@ var span1=document.createElement("SPAN");
  li.appendChild(span1);
  
  ul.appendChild(li);
+ n=mid;
+ m=mid;
+ console.log("del_n="+n);
  del.setAttribute('onclick',"delete1("+n+")");
- n++;
+ 
  edit.setAttribute('onclick',"edit("+m+")");
- m++;
+ 
 var inrval=document.getElementById("text").value;
  document.getElementById("text").value=""; 
  document.getElementById("text").focus();
  flg=0;
 ids=0;
- var today = new Date();
-    var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
+mid=0;
 
-if(dd<10) {
-    dd = '0'+dd
-} 
+}
+    }
+});
 
-if(mm<10) {
-    mm = '0'+mm
-} 
+ document.getElementById("select_type").value="select_type";
+    edit_flag=0;
+        flg=0;
+  }
+  else{
+    var type=document.getElementById("select_type").value;
+    if  (type=='Daily Task'){
+        var type_val=1;
+    }
+    else  if  (type=='Weekend Task'){
+        var type_val=2;
+    }
+    else{
+     var type_val=null;
+    }
 
-today =dd + '/'+ mm + '/' + yyyy;
-
-$.ajax({
+   $.ajax({
     type: "POST",
     url: 'http://localhost:56830/api/Todoes',
     async:true, 
     dataType:"json",
-    data:{"Description":inrval,"Status":"1","Created_At":today},
+    data:{"Description":inrval,"Status":"1","Created_At":ts.toISOString(),"Type_Id":type_val},
     crossDomain:true,
     success: function(response) {
        console.log(response);
+      mid= response["Id"];
+      mid1=response["Type_Id"];
+
+     var ul = document.getElementById("result");
+var candidate = document.getElementById("text").value;
+if (candidate==""){
+  alert("You must write Something!");
+}
+else{
+var li = document.createElement("li");
+if (ids!=0){
+  
+  var li_id="li"+ids ;
+  console.log(ids);
+}
+else{
+ 
+var li_id="li"+ mid;
+console.log("responsemid"+mid);
+}
+li.setAttribute('id',li_id);
+li.setAttribute('class',"a");
+li.appendChild(document.createTextNode(candidate));
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+var span = document.createElement("SPAN");
+var span1=document.createElement("SPAN");
+  var edit = document.createElement("button");
+  var edit_id="edit"+m;
+  edit.setAttribute('id',edit_id);
+  edit.setAttribute('value',"editor");
+  edit.setAttribute('class',"et");
+  edit.innerHTML="&#xf044";
+  span.className = "close";
+  span.appendChild(edit);
+   
+   var del = document.createElement("button");
+  var del_id="del"+n;
+  del.setAttribute('id',del_id);
+  del.setAttribute('value',"delete");
+  del.setAttribute('class',"dt");
+  del.innerHTML="&#xf014";
+ 
+  span1.className = "close";
+ span1.appendChild(del);
+ li.appendChild(span);
+ li.appendChild(span1);
+ 
+ ul.appendChild(li);
+ n=mid;
+ m=mid;
+ console.log("del_n="+n);
+ del.setAttribute('onclick',"delete1("+n+")");
+ 
+ edit.setAttribute('onclick',"edit("+m+")");
+ 
+var inrval=document.getElementById("text").value;
+ document.getElementById("text").value=""; 
+ document.getElementById("text").focus();
+ flg=0;
+ids=0;
+mid=0;
+
+}
+      
+    },
+    error:function(ERR) {
+      console.log(ERR);
     }
 });
+ document.getElementById("select_type").value="select_type"; 
 
 }
 }
-	function edit(val2){
-		
-		if (flg==1)
-		{
-		alert("You Can edit only one task at a time ");
-		document.getElementById("text").focus();
-		}else
-		{
-			flg=1;
-		var vlk2="li"+val2;
-		var rt=document.getElementById(vlk2).childNodes.item(vlk2).nodeValue;
-		document.getElementById("text").value=rt;
-		document.getElementById("text").focus();
-		delete1(val2);
-		ids=val2;
+  function edit(val2){
+   
+    if (flg==1)
+    {
+    alert("You Can edit only one task at a time ");
+    document.getElementById("text").focus();
+    }else
+    {
+           $.ajax({
+          type: "GET",
+          url: 'http://localhost:56830/api/Todoes/'+val2,
+          async:true, 
+          dataType:"json",
+          crossDomain:true,
+          success: function(response) {
+          edt_date=response["Created_At"];
+          console.log("Created_At"+edt_date);
+          edt_typeid=response["Type_Id"];
 
-		
-	}
-	
+          console.log(response["Type_Id"]);
+
+
+
+
+
+
+if (edt_typeid==1){
+           document.getElementById("select_type").selectedIndex="1";
+          
+
+           
+         }
+          else if (edt_typeid==2){
+           document.getElementById("select_type").selectedIndex="2";
+          
+          
+         }
+         else{
+          
+         document.getElementById("select_type").selectedIndex="0";
+           
+         }
+
+
+
+
+
+            }
+            });
+
+           console.log("Type_Idarun"+ edt_typeid);
+
+           
+
+      flg=1;
+    var vlk2="li"+val2;
+    console.log("id he"+val2);
+    var rt=document.getElementById(vlk2).childNodes.item(vlk2).nodeValue;
+    document.getElementById("text").value=rt;
+    document.getElementById("text").focus();
+    document.getElementById("li"+val2).remove();
+        ids=val2;
+    edit_flag=1;
+
+    
+  }
+  
 }
 
-	function delete1(val1)
-	{
-		var vlk1="li"+val1;
-		
-	document.getElementById(vlk1).remove();
-	}
-	function li_act()
-	{
-		alert("hi");
-	}
+  function delete1(val1)
+  {
+    console.log(val1);
+   
+    $.ajax({
+    type: "DELETE",
+    url: 'http://localhost:56830/api/Todoes/'+val1,
+    async:true, 
+    dataType:"json",
+    crossDomain:true,
+    success: function(response) 
+    {
 
-	function display(){
-		f1();
-		/*var xhr = new XMLHttpRequest();
-    xhr.open('GET', "http://localhost:56830/api/Todoes", true);
-    xhr.send();
+       console.log(response);
+       console.log("deletedid="+val1);
+        document.getElementById("li"+val1).remove();
 
-    xhr.addEventListener("readystatechange", processRequest, false);
-    xhr.onreadystatechange = processRequest;
-    function processRequest(e) {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var response = JSON.parse(xhr.responseText);
-            console.log(response);*/
+
+    }
+});
+    
+  }
+  function li_act()
+  {
+    alert("hi");
+  }
+
+  function display(){
+    f1();
+    
             $.ajax({
-					type: "GET",
-					url: 'http://localhost:56830/api/Todoes',
-					async:true, 
-					dataType:"json",
-					crossDomain:true,
-					success: function(response) {
-						
-					
-						console.log(response);
-           	for (let x=0;x<response.length;x++){
-           		var cand=response[x]["Description"];
-           		var idd=response[x]["Id"];
-           		cret(idd,cand);
-           	}
+          type: "GET",
+          url: 'http://localhost:56830/api/Todoes',
+          async:true, 
+          dataType:"json",
+          crossDomain:true,
+          success: function(response) {
+              for (let x=0;x<response.length;x++){
+              var cand=response[x]["Description"];
+              var idd=response[x]["Id"];
+              mid= response[x]["Id"];
+              ++mid;
+              cret(idd,cand);
+            }
+            }
+            });
+             $.ajax({
+          type: "GET",
+          url: 'http://localhost:56830/api/Todo_type',
+          async:true, 
+          dataType:"json",
+          crossDomain:true,
+          success: function(response) {
+            for (let x=0;x<response.length;x++){
+              var tsk=response[x]["Name"];
+              let tsk_id=response[x]["ID"];
+              console.log(tsk);
+              var opt=document.createElement("option");
+              opt.innerHTML=tsk;
+              opt.value=tsk_id;
+              
+              var sel=document.getElementById("select_id");
+               sel.appendChild(opt);
+
+               sel.setAttribute("onchange","get_req()");
+             
+              var opt1=document.createElement("option");
+              opt1.innerHTML=tsk;
+              opt1.value=tsk;
+              
+              var sel1=document.getElementById("select_type");
+               sel1.appendChild(opt1);
+              
+            }
+                        
+            console.log(response);
+           
+            }
+            });        
+
+
+
+
 
 
         }
-    });
-        }
-	
-	function cret(id,cand){
-		var ul = document.getElementById("result");
-		var candidate=cand;
-		console.log(candidate);
-		var li_id="li"+id;
-		var li = document.createElement("li");
-		li.setAttribute('id',li_id);
+  
+  function cret(id,cand){
+    var ul = document.getElementById("result");
+    var candidate=cand;
+    
+    var li_id="li"+id;
+    var li = document.createElement("li");
+    li.setAttribute('id',li_id);
 li.setAttribute('class',"a");
 li.appendChild(document.createTextNode(candidate));
 var myNodelist = document.getElementsByTagName("LI");
@@ -195,9 +404,62 @@ var span1=document.createElement("SPAN");
  li.appendChild(span1);
  
  ul.appendChild(li);
+ console.log("del_id="+id);
  del.setAttribute('onclick',"delete1("+id+")");
- 
+
  edit.setAttribute('onclick',"edit("+id+")");
- id++;
+
  j=m=n=id;
 }
+
+
+function get_req(){
+var optid=document.getElementById("select_id").value;
+   $.ajax({
+          type: "GET",
+          url: 'http://localhost:56830/api/Todoes',
+          async:true, 
+          dataType:"json",
+          crossDomain:true,
+          success: function(response) {
+             document.getElementById("result").innerHTML="";
+           for (let x=0;x<response.length;x++){
+              
+              var typeid= response[x]["Type_Id"];
+              
+             
+              if (typeid==optid){
+               
+                console.log(optid);
+                console.log(response[x]["Description"]);
+                var cand=response[x]["Description"];
+              var idd=response[x]["Id"];
+               mid= response[x]["Id"];
+              ++mid;
+                
+                 cret(idd,cand);
+                 //document.getElementById("li"+idd).remove();
+              }
+              else if (typeid==optid){
+
+                console.log(optid);
+                console.log(response[x]["Description"]);
+                
+                var cand=response[x]["Description"];
+              var idd=response[x]["Id"];
+               mid= response[x]["Id"];
+              ++mid;
+               /*/ var list = document.getElementById("result");
+                 while (list.hasChildNodes()) {
+                     list.removeChild(list.firstChild);
+                 }*/
+                 cret(idd,cand);
+                   //document.getElementById("li"+idd).remove();
+              } 
+            }
+            if (optid==0){
+            location.reload();
+            }
+            }
+            });
+            }
